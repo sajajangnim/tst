e3cc70f8091dc2df652cedb953eb5d20ec1d6936
sajajangnim <49239806+sajajangnim@users.noreply.github.com>
2020-10-20 03:44:07 +0700

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,13 +5,7 @@ const Header = () : ReactElement => {
   return (
     <div className={styles.container}>
       <div className={styles.title}>
-        UTS Teknologi Sistem Terintegrasi II3160
-      </div>
-      <div className={styles.title}>
-        Tafia Alifianty Dinita Putri
-      </div>
-      <div className={styles.title}>
-        18218038
+       UTS Teknologi Sistem Terintegrasi
       </div>
     </div>
   )
