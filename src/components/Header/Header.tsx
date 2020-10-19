8510ec03c772716d6c377910a16dbb94320fe841
sajajangnim <49239806+sajajangnim@users.noreply.github.com>
2020-10-20 03:35:55 +0700

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,19 +2,7 @@ import React, { ReactElement } from 'react'
 import styles from './Header.module.scss'
 
 const Header = () : ReactElement => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.title}>
-        UTS Teknologi Sistem Terintegrasi II3160
-      </div>
-      <div className={styles.title}>
-        Tafia Alifianty Dinita Putri
-      </div>
-      <div className={styles.title}>
-        18218038
-      </div>
-    </div>
-  )
+  return ()
 }
 
 export default Header
